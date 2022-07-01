a9a21054258b2f180820f6deb11c7ed65911937d
Sagar Kunwar <sagar@Sagars-MacBook-Air.local>
2022-07-01 11:59:20 -0500

    update

--- src/main/java/com/blogapp/com/postservice/PostServiceApplication.java
+++ src/main/java/com/blogapp/com/postservice/PostServiceApplication.java
@@ -3,8 +3,10 @@ package com.blogapp.com.postservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PostServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
